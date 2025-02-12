27b3ba6e3cb48a538b710eaebded1ec3b117af8f
Tyler <tylerbarrett2010+github@gmail.com>
2025-02-11 23:40:33 -0500

    get chat msgs

---
+++ static/js/get_chat.js
@@ -0,0 +1,13 @@
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", "/get_msgs/" + PAGE_NUM, true); // false for synchronous request
+let resp = xmlHttp.responseText;
+alert(resp);
+
+ol = document.getElementById("chat_msgs_ol");
+
+for (let i=0; i < resp.length; i++) {
+    msg_pkt = resp[i];
+    new_li = createElement("li");
+    new_li.innerHTML = msg_pkt[0] + ":<br />" + msg_pkt[1]
+    ol.appendChild(new_li);
+}
