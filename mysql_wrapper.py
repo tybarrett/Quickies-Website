c4c7f6bb43798a6c9674c6fc1e1ce009414a5bf7
Tyler <tylerbarrett2010+github@gmail.com>
2025-02-12 13:20:40 -0500

    prepared statements

--- mysql_wrapper.py
+++ mysql_wrapper.py
@@ -7,17 +7,18 @@ conn = mysql.connector.connect(host="127.0.0.1",
                                 auth_plugin="mysql_native_password")
 
 def get_msgs(page_num):
-    query = f"SELECT * FROM messages WHERE page_num='{page_num}';"
-    cursor = conn.cursor()
-    cursor.execute(query)
+    query = f"SELECT * FROM messages WHERE page_num=%s;"
+    cursor = conn.cursor(prepared=True)
+    cursor.execute(query, (page_num, ))
     results = cursor.fetchall()
     print(results)
     return results
 
 
 def put_msg(data):
-    query = f"INSERT INTO messages (user_name, message, page_num) VALUES ('{data['name']}', '{data['msg']}', '{data['page_num']}');"
-    cursor = conn.cursor()
-    cursor.execute(query)
+    # query = f"INSERT INTO messages (user_name, message, page_num) VALUES ('{data['name']}', '{data['msg']}', '{data['page_num']}');"
+    query = f"INSERT INTO messages (user_name, message, page_num) VALUES (%s, %s, %s');"
+    cursor = conn.cursor(prepared=True)
+    cursor.execute(query, (data["name"], data["msg"], data["page_num"]))
     conn.commit()
     cursor.close()
